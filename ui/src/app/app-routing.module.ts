971eac5a86392da5c70066dd94aefd561764c794
= <=>
2021-05-24 20:25:27 +1000

    Preparation addkey component

--- ui/src/app/app-routing.module.ts
+++ ui/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { KeysComponent } from './keys/keys.component';
 import { LoginComponent } from './login/login.component';
 import { RegistrationComponent } from './registration/registration.component';
+import {AddkeyComponent} from './addkey/addkey.component'
 
 const routes: Routes = [
       { path: 'keys', component: KeysComponent },
       { path: 'Login', component: LoginComponent },
       { path: 'Registration', component: RegistrationComponent },
+      { path: 'Addkey', component: AddkeyComponent },
       { path: '', redirectTo: 'Login', pathMatch: 'full' }
   ];
 
