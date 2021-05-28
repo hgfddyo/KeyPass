3763b343cf97a6c2845f1e376151a4f53924bafc
= <=>
2021-05-28 18:39:19 +1000

    component logic

--- ui/src/app/keystable/keystable.component.ts
+++ ui/src/app/keystable/keystable.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild, TemplateRef, Inject, Input, AfterViewInit } from '@angular/core';
+import { Component, OnInit, ViewChild, TemplateRef, Inject, Input, AfterViewInit, QueryList, ViewChildren } from '@angular/core';
 import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { Account } from '../account'
 import { KeyringService } from '../keyring.service'
@@ -8,6 +8,9 @@ import { Router } from '@angular/router';
 import { FormControl, Validators } from '@angular/forms'
 import {MatTableDataSource} from '@angular/material/table';
 import {MatPaginator} from '@angular/material/paginator';
+import {MatInput} from '@angular/material/input'
+import {MatCheckbox} from '@angular/material/checkbox'
+import { MatButton } from '@angular/material/button';
 
 
 @Component({
@@ -19,16 +22,17 @@ export class KeystableComponent implements OnInit {
 
   displayedColumns: string[] = ['context', 'login', 'password', 'show', 'edit', 'delete'];
   dataSource:any
-  check:boolean
+  
 
+  @ViewChildren(MatInput) matInputs: QueryList<MatInput>;
   @ViewChild(MatPaginator) paginator: MatPaginator;
 
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
   }
+
   constructor(private keyringService : KeyringService) {
-    this.check = false
     this.keyringService.getKeyRing().subscribe(result =>{
       this.dataSource = new MatTableDataSource(result);
       this.dataSource.paginator = this.paginator;
@@ -36,13 +40,36 @@ export class KeystableComponent implements OnInit {
         return data.context.trim().toLocaleLowerCase().indexOf(filterValue.trim().toLocaleLowerCase()) >= 0 ||
         data.login.trim().toLocaleLowerCase().indexOf(filterValue.trim().toLocaleLowerCase()) >= 0;
       };
-      
-    })
-    
+    }) 
   }
 
   show(element){
-    this.check = true
+    let editIcons =[].filter.call(document.getElementsByTagName('mat-icon'), 
+      el => el.id == element.context + element.login)
+    const input = this.matInputs.find(
+      matInput => matInput.id === element.context + element.login
+    );
+    if(input.type === 'text'){
+      input.type = 'password'
+      editIcons[0].innerText='visibility'
+    } else{
+        input.type = 'text'
+        editIcons[0].innerText='visibility_off'   
+    }
+    
+  }
+
+  deleteLogin(element){
+    this.keyringService.deleteKey({
+      login: element.login,
+      context: element.context,
+      password: element.password
+    })
+    setTimeout(() => {
+      this.keyringService.getKeyRing().subscribe(result =>{
+        this.dataSource.data = result
+    })
+    },100)
   }
 
   ngOnInit(): void {
